956a9aa3840aa494535ff2e928d519c670eceb15
Nick Chadwick <nick.chadwick@noaa.gov>
2025-05-09 11:47:43 -0500

    deployment fixes

--- Core/LAMBDA/viz_functions/zonal_coastal_fim_processing/main.tf
+++ Core/LAMBDA/viz_functions/zonal_coastal_fim_processing/main.tf
@@ -85,7 +85,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux-x86_64-standard:3.0"
+    image                       = "aws/codebuild/standard:6.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
