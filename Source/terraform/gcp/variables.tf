4da0b98d2d2f0cc767a944d24cc54ea18102514f
Tadd Bindas <Tadd.N.Bindas@rtx.com>
2025-09-13 13:22:25 -0400

    feat: added reference to terraform IaC (#37)

--- Source/terraform/gcp/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "GCP project where resources will be created."
-  type        = string
-}
-
-variable "region" {
-  description = "GCP region where resources will be created."
-  type        = string
-}
